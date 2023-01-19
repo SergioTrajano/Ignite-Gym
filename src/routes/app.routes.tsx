3176e910f70c1b88bd6c1211180340922376e8f6
SergioTrajano <sergio.trajanosilva2001@gmail.com>
2023-01-19 19:18:35 -0300

    :sparkles: feat: hide tab bar labels

--- src/routes/app.routes.tsx
+++ src/routes/app.routes.tsx
@@ -18,7 +18,7 @@ const { Navigator, Screen } = createBottomTabNavigator<AppRoutesProps>();
 
 export function AppRoutes() {
     return (
-        <Navigator screenOptions={{ headerShown: false }}>
+        <Navigator screenOptions={{ headerShown: false, tabBarShowLabel: false }}>
             <Screen
                 name="home"
                 component={Home}
