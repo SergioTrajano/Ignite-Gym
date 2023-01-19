fcbcc450fa09b021ca54dd5d94599416a58f5691
SergioTrajano <sergio.trajanosilva2001@gmail.com>
2023-01-19 19:17:05 -0300

    :sparkles: feat: create private routes

---
+++ src/routes/app.routes.tsx
@@ -0,0 +1,43 @@
+import { createBottomTabNavigator, BottomTabNavigationProp } from "@react-navigation/bottom-tabs";
+
+import { Exercise } from "@screens/Exercise";
+import { History } from "@screens/History";
+import { Home } from "@screens/Home";
+import { Profile } from "@screens/Profile";
+
+type AppRoutesProps = {
+    home: undefined;
+    history: undefined;
+    profile: undefined;
+    exercise: undefined;
+};
+
+export type NavigatorAppRoutesProps = BottomTabNavigationProp<AppRoutesProps>;
+
+const { Navigator, Screen } = createBottomTabNavigator<AppRoutesProps>();
+
+export function AppRoutes() {
+    return (
+        <Navigator screenOptions={{ headerShown: false }}>
+            <Screen
+                name="home"
+                component={Home}
+            />
+
+            <Screen
+                name="history"
+                component={History}
+            />
+
+            <Screen
+                name="profile"
+                component={Profile}
+            />
+
+            <Screen
+                name="exercise"
+                component={Exercise}
+            />
+        </Navigator>
+    );
+}
