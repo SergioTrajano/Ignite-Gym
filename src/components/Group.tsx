6f589f5838a56de589729862b5920552c8ce637a
SergioTrajano <sergio.trajanosilva2001@gmail.com>
2023-01-20 02:42:15 -0300

    :sparkles: feat: create Group component

---
+++ src/components/Group.tsx
@@ -0,0 +1,30 @@
+import { Text, Pressable, IPressableProps } from "native-base";
+
+type Props = IPressableProps & {
+    name: string;
+};
+
+export function Group({ name, ...rest }: Props) {
+    return (
+        <Pressable
+            width={24}
+            height={10}
+            backgroundColor="gray.600"
+            rounded="md"
+            justifyContent="center"
+            alignItems="center"
+            marginRight={3}
+            overflow="hidden"
+            {...rest}
+        >
+            <Text
+                color="gray.200"
+                fontSize="xs"
+                textTransform="uppercase"
+                fontWeight="bold"
+            >
+                {name}
+            </Text>
+        </Pressable>
+    );
+}
