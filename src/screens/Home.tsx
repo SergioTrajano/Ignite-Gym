ecb62d08c47adbc26ea54859d2c7f744aea5d9f1
SergioTrajano <sergio.trajanosilva2001@gmail.com>
2023-01-20 03:00:23 -0300

    :sparkles: feat: add Group component to Home screen

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,10 +1,27 @@
+import { HStack, VStack } from "native-base";
+
 import { HomeHeader } from "@components/HomeHeader";
-import { VStack } from "native-base";
+import { Group } from "@components/Group";
+import { useState } from "react";
 
 export function Home() {
+    const [groupSelect, setGroupSelected] = useState<string>("costa");
     return (
         <VStack>
             <HomeHeader />
+
+            <HStack>
+                <Group
+                    name="costas"
+                    isActive={groupSelect === "costas"}
+                    onPress={() => setGroupSelected("costas")}
+                />
+                <Group
+                    name="ombro"
+                    isActive={groupSelect === "ombro"}
+                    onPress={() => setGroupSelected("ombro")}
+                />
+            </HStack>
         </VStack>
     );
 }
