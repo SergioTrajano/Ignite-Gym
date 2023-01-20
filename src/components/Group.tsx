9b051a6f553414e7014f3cb08564b10a3aeafa48
SergioTrajano <sergio.trajanosilva2001@gmail.com>
2023-01-20 02:57:34 -0300

    :sparkles: feat: add Text styles when Group is pressed

--- src/components/Group.tsx
+++ src/components/Group.tsx
@@ -2,9 +2,10 @@ import { Text, Pressable, IPressableProps } from "native-base";
 
 type Props = IPressableProps & {
     name: string;
+    isActive: boolean;
 };
 
-export function Group({ name, ...rest }: Props) {
+export function Group({ name, isActive, ...rest }: Props) {
     return (
         <Pressable
             width={24}
@@ -15,6 +16,7 @@ export function Group({ name, ...rest }: Props) {
             alignItems="center"
             marginRight={3}
             overflow="hidden"
+            isPressed={isActive}
             _pressed={{
                 borderColor: "green.500",
                 borderWidth: 1,
@@ -22,7 +24,7 @@ export function Group({ name, ...rest }: Props) {
             {...rest}
         >
             <Text
-                color="gray.200"
+                color={isActive ? "green.500" : "gray.200"}
                 fontSize="xs"
                 textTransform="uppercase"
                 fontWeight="bold"
