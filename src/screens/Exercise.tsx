972743d013b59a88cf7dd36a384facd959a364be
SergioTrajano <sergio.trajanosilva2001@gmail.com>
2023-01-23 04:26:59 -0300

    :sparkles: feat: add icon to navigate back to home screen

--- src/screens/Exercise.tsx
+++ src/screens/Exercise.tsx
@@ -1,9 +1,33 @@
-import { Center, Text } from "native-base";
+import { useNavigation } from "@react-navigation/native";
+
+import { TouchableOpacity } from "react-native";
+import { Icon, VStack } from "native-base";
+import { Feather } from "@expo/vector-icons";
+
+import { NavigatorAppRoutesProps } from "@routes/app.routes";
 
 export function Exercise() {
+    const { goBack } = useNavigation<NavigatorAppRoutesProps>();
+
+    function handleGoBack() {
+        goBack();
+    }
     return (
-        <Center flex={1}>
-            <Text color={"white"}>Tela Exercise</Text>
-        </Center>
+        <VStack flex={1}>
+            <VStack
+                paddingX={8}
+                backgroundColor="gray.600"
+                paddingTop={12}
+            >
+                <TouchableOpacity onPress={handleGoBack}>
+                    <Icon
+                        as={Feather}
+                        name="arrow-left"
+                        color="green.500"
+                        size={6}
+                    />
+                </TouchableOpacity>
+            </VStack>
+        </VStack>
     );
 }
