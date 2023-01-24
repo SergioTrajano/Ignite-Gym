d4e7656eb388b5a4b3a2d23ec8617973e4690120
SergioTrajano <sergio.trajanosilva2001@gmail.com>
2023-01-23 22:58:59 -0300

    :sparkles: feat: implement interceptor to deal with error

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,5 +1,20 @@
 import axios from "axios";
 
-export const api = axios.create({
-    baseURL: process.env.API_URL,
+import { AppError } from "@utils/AppError";
+
+const api = axios.create({
+    baseURL: "http://192.168.1.14:3333",
 });
+
+api.interceptors.response.use(
+    (response) => response,
+    (error) => {
+        if (error.response && error.response.data) {
+            return Promise.reject(new AppError(error.response.data.message));
+        } else {
+            return Promise.reject(error);
+        }
+    }
+);
+
+export { api };
