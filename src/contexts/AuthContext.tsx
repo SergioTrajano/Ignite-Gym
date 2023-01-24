1c697788f3e1ee1539b39bdd9d537904bc71332a
SergioTrajano <sergio.trajanosilva2001@gmail.com>
2023-01-24 01:32:57 -0300

    :sparkles: feat: update context based on api response

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -1,10 +1,12 @@
 import { createContext, ReactNode, useState } from "react";
 
+import { api } from "@services/api";
+
 import { UserDTO } from "@dtos/UserDTO";
 
 export type AuthContextDataProps = {
     user: UserDTO;
-    signIn: (email: string, password: string) => void;
+    signIn: (email: string, password: string) => Promise<void>;
 };
 
 type AuthContextProviderProps = {
@@ -16,13 +18,14 @@ export const AuthContext = createContext<AuthContextDataProps>({} as AuthContext
 export function AuthContextProvider({ children }: AuthContextProviderProps) {
     const [user, setUser] = useState<UserDTO>({} as UserDTO);
 
-    function signIn(email: string, password: string) {
-        setUser({
-            id: "",
-            name: "",
-            email,
-            avatar: "",
-        });
+    async function signIn(email: string, password: string) {
+        try {
+            const { data } = await api.post("/sessions", { email, password });
+
+            setUser(data.user);
+        } catch (error) {
+            throw error;
+        }
     }
 
     return <AuthContext.Provider value={{ user, signIn }}>{children}</AuthContext.Provider>;
