2b5c1b24045efbf7dacb1c583502abb07e4397fc
SergioTrajano <sergio.trajanosilva2001@gmail.com>
2023-01-29 01:26:48 -0300

    :sparkles: feat: implements refresh token
    - Check if requestError is because of token
    - if error is a token error but is not a refresh token error, signOut user
    - if error is refresh token error:
        -> check if token is null, if is null signOut user
        -> Else save request in queue, start refresh process

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,6 +1,9 @@
 import axios, { AxiosInstance } from "axios";
 
+import { storageToken } from "@storage/storageAuthToken";
+
 import { AppError } from "@utils/AppError";
+import { reject } from "lodash";
 
 type SignOut = () => void;
 
@@ -8,18 +11,100 @@ type APIInstaceProps = AxiosInstance & {
     registerInterceptTokenManager: (signOut: SignOut) => () => void;
 };
 
+type PromiseType = {
+    resolve: (value?: unknown) => void;
+    reject: (reason?: unknown) => void;
+};
+
+type ProcessQueueParams = {
+    error: Error | null;
+    token: string | null;
+};
+
 const api = axios.create({
     baseURL: "http://192.168.1.14:3333",
 }) as APIInstaceProps;
 
+let isRefreshing = false;
+let failedQueue: Array<PromiseType> = [];
+
+const process = ({ error, token = null }: ProcessQueueParams): void => {
+    failedQueue.forEach((request) => {
+        if (error) {
+            request.reject(error);
+        } else {
+            request.resolve(token);
+        }
+    });
+
+    failedQueue = [];
+};
+
 api.registerInterceptTokenManager = (signOut) => {
     const interceptTokenManager = api.interceptors.response.use(
         (response) => response,
-        (error) => {
-            if (error.response && error.response.data) {
-                return Promise.reject(new AppError(error.response.data.message));
+        async (requestError) => {
+            if (requestError?.response?.status === 401) {
+                if (
+                    requestError.response.data?.message === "token.expired" ||
+                    requestError.response.data?.message === "token.invalid"
+                ) {
+                    const currentToken = await storageToken.get();
+
+                    if (currentToken === null) {
+                        signOut();
+                        return Promise.reject(requestError);
+                    }
+
+                    const originalRequest = requestError.config;
+
+                    if (isRefreshing) {
+                        return new Promise((resolve, reject) => {
+                            failedQueue.push({ resolve, reject });
+                        })
+                            .then((token) => {
+                                originalRequest.headers["Authorization"] = `Bearer ${token}`;
+
+                                return axios(originalRequest);
+                            })
+                            .catch((error) => {
+                                throw error;
+                            });
+                    }
+
+                    isRefreshing = true;
+
+                    return new Promise(async (resolve, reject) => {
+                        try {
+                            const { data } = await api.post("/sessions/refresh-token", {
+                                token: currentToken,
+                            });
+
+                            await storageToken.save(data.token);
+
+                            api.defaults.headers.common["Authorization"] = `Bearer ${data.token}`;
+                            originalRequest.headers["Authorization"] = `Bearer ${data.token}`;
+
+                            process({ error: null, token: data.token });
+                            resolve(originalRequest);
+                        } catch (error: any) {
+                            process({ error, token: null });
+                            signOut();
+
+                            reject(error);
+                        } finally {
+                            isRefreshing = false;
+                        }
+                    });
+                }
+
+                signOut();
+            }
+
+            if (requestError.response && requestError.response.data) {
+                return Promise.reject(new AppError(requestError.response.data.message));
             } else {
-                return Promise.reject(error);
+                return Promise.reject(requestError);
             }
         }
     );
